579fa797f68e976ea3394a7449acee071dcce4bc
Gabriel <gabrielpabalesalazar@gmail.com>
2024-04-17 22:01:58 +0800

    added some stuff in todo.domain (Category and the ToDo model)

---
+++ ToDo.Domain/ToDo.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Domain
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description.")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a due date.")]
+        public DateTime? DueDate { get; set; }
+
+        [Required(ErrorMessage = "Please select a category.")]
+        public string CategoryId { get; set; } = string.Empty;
+
+        public Category Category { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please select a status.")]
+
+        public string Status { get; set; } = string.Empty;
+
+        public bool Done { get; set; } = false;
+    }
+}
