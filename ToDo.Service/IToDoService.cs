def81a5f3a3c9ab103d9e7173217cdf063440431
smarty0917 <martintristanmreyes@gmail.com>
2024-04-17 23:43:14 +0800

    added service

---
+++ ToDo.Service/IToDoService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ToDo.Domain
+{
+    public interface IToDoService
+    {
+        Task<List<ToDo>> GetAllAsync();
+        Task<ToDo> GetByIdAsync(int id);
+        Task AddAsync(ToDo toDo);
+        Task UpdateAsync(ToDo toDo);
+        Task DeleteAsync(int id);
+    }
+}
