fe791fae2f57b33f90da49d73bf394aae1766bf5
Rodejo_VaronIan_BSIT32E1 <varonianrodejo@gmail.com>
2024-04-17 23:40:57 +0800

    Created non fuctional repositories (*^▽^*)

---
+++ ToDo.Repositories/staticRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using ToDo.Domain.Interface;
+using ToDo.Repositories;
+namespace ToDo.Repositories
+{
+    public class staticRepository : ITodoRepository
+    {
+        
+        public Domain.ToDo Add(Domain.ToDo toDo)
+        {
+            if(toDo == null)
+            {
+                throw new ArgumentNullException(nameof(toDo));
+            }
+
+            
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Domain.ToDo> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.ToDo> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.ToDo Update(Domain.ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
