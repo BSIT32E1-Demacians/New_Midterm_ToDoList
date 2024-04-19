583ac98c1158a1fb910cafb6adf1702500f2555f
Rodejo_VaronIan_BSIT32E1 <varonianrodejo@gmail.com>
2024-04-19 19:46:56 +0800

    1

--- ToDo.Repositories/staticRepository.cs
+++ ToDo.Repositories/staticRepository.cs
@@ -1,45 +1,49 @@
-﻿using System.ComponentModel;
-using ToDo.Domain.Interface;
-using ToDo.Repositories;
-namespace ToDo.Repositories
-{
-    public class staticRepository : ITodoRepository
-    {
-        
-        public Domain.ToDo Add(Domain.ToDo toDo)
-        {
-            if(toDo == null)
-            {
-                throw new ArgumentNullException(nameof(toDo));
-            }
+﻿using System.Collections.Generic;
+using System.Linq;
+using ToDo.Domain;
 
-            
-            throw new NotImplementedException();
+namespace ToDo.Repository
+{
+    public class ToDoRepository
+    {
+        private readonly List<Domain.ToDo> _todos = new List<Domain.ToDo> ();
+        private int _nextId = 1;
+
+        public Domain.ToDo Create(Domain.ToDo todo)
+        {
+            todo.Id = _nextId++;
+            _todos.Add(todo);
+            return todo;
+        }
+
+        public Domain.ToDo GetById(int id)
+        {
+            return _todos.FirstOrDefault(t => t.Id == id);
         }
 
         public IEnumerable<Domain.ToDo> GetAll()
         {
-            throw new NotImplementedException();
+            return _todos.ToList();
         }
 
-        public Task<Domain.ToDo> GetByIdAsync(int id)
+        public void Update(Domain.ToDo updatedTodo)
         {
-            throw new NotImplementedException();
+            var existingTodo = GetById(updatedTodo.Id);
+            if (existingTodo != null)
+            {
+                existingTodo.Description = updatedTodo.Description;
+                existingTodo.Category = updatedTodo.Category;
+                existingTodo.Done = updatedTodo.Done;
+            }
         }
 
-        public bool Remove(int id)
+        public void Delete(int id)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Domain.ToDo Update(Domain.ToDo toDo)
-        {
-            throw new NotImplementedException();
+            var todoToRemove = GetById(id);
+            if (todoToRemove != null)
+            {
+                _todos.Remove(todoToRemove);
+            }
         }
     }
 }
