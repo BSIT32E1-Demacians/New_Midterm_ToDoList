2790d603bde592de11abf79136fc389412cdb71e
GarciaMarkAngelo-BSIT32E1 <160812645+GarciaMarkAngelo-BSIT32E1@users.noreply.github.com>
2024-04-19 21:47:41 +0800

    Created AppDbContext and its functions

---
+++ Midterm_ToDoList/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Midterm_ToDoList.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<NToDo> NToDo { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NToDo>().HasData(
+
+                new NToDo { Id = 1, Description = "Generating Art", Category = "Activity", Done = false }
+                );
+        }
+
+    }
+}
