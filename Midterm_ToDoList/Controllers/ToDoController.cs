b4f72a02ed0a3865ba84c22801adc639023428be
Rodejo_VaronIan_BSIT32E1 <varonianrodejo@gmail.com>
2024-04-19 20:09:35 +0800

    31

--- Midterm_ToDoList/Controllers/ToDoController.cs
+++ Midterm_ToDoList/Controllers/ToDoController.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ToDo.Services;
+using ToDo.Repository;
+using ToDo.Domain;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Midterm_ToDoList.Controllers
+public class ToDoController : Controller
 {
-    public class ToDoController : Controller
+    private readonly ToDoService _toDoService;
+
+    public ToDoController(ToDoService toDoService)
     {
-        public IActionResult Index()
+        _toDoService = toDoService;
+    }
+
+    public ActionResult Index()
+    {
+        var todos = _toDoService.GetAll();
+        return View(todos);
+    }
+
+    [HttpGet]
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(ToDo.Domain.ToDo todo)
+    {
+        if (string.IsNullOrWhiteSpace(todo.Description) || string.IsNullOrWhiteSpace(todo.Category))
         {
-            return View();
+            ModelState.AddModelError("", "Fields must not be empty");
+            return View(todo);
         }
+        _toDoService.Create(todo);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public ActionResult Edit(int id)
+    {
+        var todo = _toDoService.GetById(id);
+        return View(todo);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(ToDo.Domain.ToDo todo)
+    {
+        if (string.IsNullOrWhiteSpace(todo.Description) || string.IsNullOrWhiteSpace(todo.Category))
+        {
+            ModelState.AddModelError("", "Fields must not be empty");
+            return View(todo);
+        }
+        _toDoService.Update(todo);
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+        _toDoService.Delete(id);
+        return RedirectToAction("Index");
     }
 }
