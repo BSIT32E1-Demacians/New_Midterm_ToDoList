776fbb3a3f5f410481cf41e20821f09980ac7fa7
GarciaMarkAngelo-BSIT32E1 <160812645+GarciaMarkAngelo-BSIT32E1@users.noreply.github.com>
2024-04-19 21:34:44 +0800

    Added NTodo model.

---
+++ Midterm_ToDoList/Models/NToDo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Midterm_ToDoList.Models
+{
+    public class NToDo
+    {
+
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public bool Done { get; set; } = false;
+
+    }
+}
